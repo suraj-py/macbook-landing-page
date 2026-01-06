e0be54ab0b590b1d7870933ef2e5eee2fda5f369
Suraj Mhatre <surajmcodes@gmail.com>
2026-01-06 16:14:23 +0530

    feat: ProductViewer component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   );
 };
