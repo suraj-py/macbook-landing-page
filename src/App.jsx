40fe3c79b0333694cc9e7e6a668349888f58e926
Suraj Mhatre <surajmcodes@gmail.com>
2026-01-10 23:52:21 +0530

    feat: switching between 14 inch and 16 inch model feature added using gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -3,10 +3,13 @@ import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
 import MacbookModel14 from "./models/Macbook-14";
-import StudioLights from "./StudioLights";
+import StudioLights from "./three/StudioLights";
+import ModelSwitcher from "./three/ModelSwitcher";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
   return (
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
@@ -65,7 +68,10 @@ const ProductViewer = () => {
         <StudioLights />
         <MacbookModel14 position={[0, 0, 0]} scale={0.06} />
 
-        <OrbitControls enableZo0m={false} />
+        <ModelSwitcher
+          scale={isMobile ? scale - 0.03 : scale}
+          isMobile={isMobile}
+        />
       </Canvas>
     </section>
   );
